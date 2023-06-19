9ec3152623f856f3a38f6c3e3dd77dac6e86c7ff
Aren Qamalyan <qamalyanaren@gmail.com>
2023-06-19 11:30:50 +0400

    feat: add logic to emmit flows every 1000 seconds

--- app/src/main/java/am/solidy/flowsummator/MainActivity.kt
+++ app/src/main/java/am/solidy/flowsummator/MainActivity.kt
@@ -1,16 +1,29 @@
 package am.solidy.flowsummator
 
+import am.solidy.flowsummator.ui.theme.FlowSimulatorTheme
+import am.solidy.flowsummator.ui.theme.Typography
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import am.solidy.flowsummator.ui.theme.FlowSimulatorTheme
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +43,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    viewModel: MainViewModel = viewModel(),
+) {
+
+    val inputText = remember { viewModel.summatorInputValue }.value
+    val summatorResult = viewModel.summatorResult.collectAsState()
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(top = 64.dp),
+            text = "Flow summator",
+            style = Typography.titleLarge
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            TextField(
+                value = inputText,
+                label = { Text(text = "Write count of flows") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                onValueChange = { it ->
+                    viewModel.setInputValue(it)
+                }
+            )
+            Button(
+                modifier = Modifier.padding(top = 8.dp),
+                onClick = {
+                    viewModel.startSummator()
+                },
+                enabled = inputText.text.isNotBlank()
+            ) {
+                Text(text = "Start Summator")
+            }
+
+            Text(
+                modifier = Modifier.padding(top = 16.dp),
+                text = summatorResult.value.joinToString(" "),
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FlowSimulatorTheme {
-        MainScreen("Android")
+        MainScreen(
+            viewModel = viewModel()
+        )
     }
 }
--- app/src/main/java/am/solidy/flowsummator/MainViewModel.kt
+++ app/src/main/java/am/solidy/flowsummator/MainViewModel.kt
@@ -1,9 +1,86 @@
 package am.solidy.flowsummator
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 
 class MainViewModel : ViewModel() {
 
+    private val _summatorInputValue = mutableStateOf(TextFieldValue(""))
+    val summatorInputValue: State<TextFieldValue> = _summatorInputValue
 
+    fun setInputValue(value: TextFieldValue) {
+        _summatorInputValue.value = value
+    }
+
+    private val _summatorResult = MutableStateFlow<List<Int>>(mutableListOf())
+    val summatorResult = _summatorResult.asStateFlow()
+
+
+    private var summatorJob: Job? = null
+    fun startSummator() {
+        summatorJob?.cancel()
+        summatorJob = viewModelScope.launch {
+            val flowsCount = summatorInputValue.value.text.toInt()
+            val listOfFlow = mutableListOf<Flow<Int>>()
+            repeat(flowsCount) { index ->
+                val currentFlow = flow {
+                    val value = index + 1
+                    delay(FLOW_EMMIT_DELAY_TIME * value)
+                    emit(value)
+                }
+                listOfFlow.add(currentFlow)
+            }
+
+            //first approach
+            listOfFlow.merge().fold(0) { a, b ->
+                val sumOfEmmitingValue = a + b
+                updateSummatorResult(sumOfEmmitingValue)
+                sumOfEmmitingValue
+            }
+
+            //second approach
+//            channelFlow {
+//                listOfFlow.forEach { flow ->
+//                    launch {
+//                        flow.onEach { send(it) }.launchIn(this)
+//                    }
+//                }
+//            }.fold(0) { a, b ->
+//                val sumOfEmmitingValue = a + b
+//                updateSummatorResult(sumOfEmmitingValue)
+//                sumOfEmmitingValue
+//            }
+        }
+    }
+
+    private fun updateSummatorResult(item: Int) {
+        _summatorResult.update {
+            val newList = it.toMutableList()
+            newList.add(item)
+            newList
+        }
+    }
+
+    override fun onCleared() {
+        summatorJob?.cancel()
+        super.onCleared()
+    }
+
+    companion object {
+        private const val FLOW_EMMIT_DELAY_TIME = 1000L
+    }
 }
